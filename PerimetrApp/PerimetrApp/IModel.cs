fd815d27c9a16fa162749d31d65c0ac97383078a
SergoDobro <optimgrey@yandex.ru>
2021-02-11 20:57:07 +0300

    Added IModel

---
+++ PerimetrApp/PerimetrApp/IModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerimetrApp
+{
+    /// <summary>
+    /// Protocol for developing the perimetr counting class
+    /// </summary>
+    interface IModel
+    {
+        /// <summary>
+        /// Method which returns perimetr of rectangle by 2 sides
+        /// </summary>
+        /// <param name="a">One side of the rectangle</param>
+        /// <param name="b">Other side of the rectangle</param>
+        /// <returns></returns>
+        int GetP(int a, int b);
+    }
+}
